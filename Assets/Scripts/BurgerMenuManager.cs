5d774bdca2eed6509e155daf686419ca2c2b0af3
KnightDIRT <knightkantinan@gmail.com>
2023-10-08 17:57:07 +0700

    Updated BurgerMenuManager.cs
    Now Display Icon From Prefab

--- Assets/Scripts/BurgerMenuManager.cs
+++ Assets/Scripts/BurgerMenuManager.cs
@@ -9,15 +9,19 @@ public class BurgerMenuManager : MonoBehaviour
 {
     [SerializeField] Burger targetBurger;
 
-    private void Awake()
+    private void Start()
     {
         foreach (BurgerPart burgerPart in instanceBurgerManager.burgerPartPrefabs.Skip(2))
         {
             var iconUI = new GameObject();
             iconUI.name = burgerPart.name;
-            iconUI.transform.parent = transform.Find("Scroll View").Find("Viewport").Find("Content");
             iconUI.AddComponent<RawImage>();
             iconUI.GetComponent<RawImage>().texture = burgerPart.icon;
+            iconUI.transform.parent = transform.Find("Scroll View").Find("Viewport").Find("Content");
+            var iconTransform = iconUI.GetComponent<RectTransform>();
+            iconTransform.anchoredPosition3D = Vector3.zero;
+            iconTransform.localRotation = Quaternion.identity;
+            iconTransform.localScale = Vector3.one;
         }
     }
 
