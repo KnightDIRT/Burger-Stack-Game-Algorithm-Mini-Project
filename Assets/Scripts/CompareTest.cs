0dfbcb0acb47a1504ca3b900e32e3852d97ad323
KnightDIRT <knightkantinan@gmail.com>
2023-10-09 20:24:32 +0700

    Added CompareTest.cs

---
+++ Assets/Scripts/CompareTest.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static BurgerManager;
+
+public class CompareTest : MonoBehaviour
+{
+    [SerializeField] bool compare;
+    [SerializeField] Burger burger1;
+    [SerializeField] Burger burger2;
+
+    void Update()
+    {
+        if (compare)
+        {
+            var output = instanceBurgerManager.CompareBurger(burger1, burger2);
+            Debug.Log(string.Format("Mismatch: {0}\nCount Mismatch: {1}", output.ToString()));
+
+            compare = false;
+        }
+    }
+}
